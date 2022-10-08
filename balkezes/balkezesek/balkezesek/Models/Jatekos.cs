787021b6ec7d1ff9e973762e8f2bcb67db703058
Toth Mihaly <98757199+sze1totmih@users.noreply.github.com>
2022-10-08 12:49:11 +0200

    Add files via upload

---
+++ balkezes/balkezesek/balkezesek/Models/Jatekos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balkezesek.models
+{
+    public class Jatekos
+    {
+        
+        public String Name { get; set; }
+        public DateTime ElsoPalyaraLepes { get; set; }
+        public DateTime UtolsoPalyaraLepes { get; set; }
+        public int Suly { get; set; }
+        public int Magassag { get; set; }
+
+        public Jatekos(string[] sor)
+        {
+            Name = sor[0];
+            ElsoPalyaraLepes = Convert.ToDateTime(sor[1]);
+            UtolsoPalyaraLepes = Convert.ToDateTime(sor[2]);
+            Suly = Convert.ToInt32(sor[3]);
+            Magassag = Convert.ToInt32(sor[4]);
+        }
+
+        public Jatekos(string name, DateTime elsoPalyaraLepes, DateTime utolsoPalyaraLepes, int suly, int magassag)
+        {
+            Name = name;
+            ElsoPalyaraLepes = elsoPalyaraLepes;
+            UtolsoPalyaraLepes = utolsoPalyaraLepes;
+            Suly = suly;
+            Magassag = magassag;
+        }
+    }
+}
---
+++ balkezes/balkezesek/balkezesek/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using balkezesek.models;
+using System.Linq;
+
+
+namespace balkezesek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Jatekos> balkezesek = new List<Jatekos>();
+
+            #region 2. feladat
+            using (var sr = new StreamReader("balkezesek.csv"))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string[] sor = sr.ReadLine().Split(';');
+                    balkezesek.Add(new Jatekos(sor));
+                }
+            }
+
+            #endregion
+
+            #region 3. feladat
+            Console.WriteLine("3. feladat" + " "+ balkezesek.Count);
+            #endregion
+
+            #region 4. feladat
+            Console.WriteLine("4. feladat");
+            var oktoberi99 = balkezesek.Where(x => x.UtolsoPalyaraLepes.Year == 1999 && x.UtolsoPalyaraLepes.Month == 10).ToList();
+            oktoberi99.ForEach(x => Console.WriteLine($"{x.Name}, {Math.Round(x.Magassag*2.54, 1)} cm"));
+            Console.WriteLine();
+            #endregion
+
+            #region 5. feladat
+            Console.WriteLine("5. feladat");
+            Console.WriteLine("Kérek egy évszámot 1990 és 1999 közötti évszámot!: ");
+            int evszam = 0;
+            do
+            {
+                var input = Console.ReadLine();
+                int.TryParse(input, out evszam);
+                if (evszam >= 1990 && evszam <= 1999) { break;}
+                Console.WriteLine("Hibás adat, kérek egy évszámot 1990 és 1999 közötti évszámot!: ");
+            } while (evszam < 1990 || evszam > 1999);
+            #endregion
+
+            #region 6. feladat
+            var atlagsuly = balkezesek.Where(x => x.ElsoPalyaraLepes.Year <= evszam && x.UtolsoPalyaraLepes.Year >= evszam).Average(x => x.Suly);
+            Console.WriteLine("6. feladat " + Math.Round(atlagsuly, 2) + " font");
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
